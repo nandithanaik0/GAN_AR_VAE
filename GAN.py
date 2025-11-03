0498119135e0f1845b51bfd6fbfea6986530afb0
Nanditha Naik <82931456+nandithanaik0@users.noreply.github.com>
2025-11-03 09:02:45 -0600

    Update GAN.py

--- GAN.py
+++ GAN.py
@@ -6,15 +6,8 @@ class Discriminator(nn.Module):
     def __init__(self, channels = [256, 128, 64]):
         super().__init__()
         
-        ##################
-        # TODO: Implement the Discriminator architecture
-        # Hint: use three fully connected layers with LeakyReLU activations
-        #       and dropout as a form of regularization. The final output 
-        #       should be a single logit (no Sigmoid).
 
-        ##################
         self.model = nn.Sequential(
-            # TODO: fill in layers here
             nn.Linear(28*28, channels[0]),
             nn.LeakyReLU(0.25),
             nn.Dropout(0.3),
@@ -28,7 +21,7 @@ class Discriminator(nn.Module):
             nn.Dropout(0.3),
 
             nn.Linear(channels[2], 1),
-            nn.Identity()  # placeholder
+            nn.Identity()  
         )
 
     def forward(self, x):
@@ -41,12 +34,6 @@ class Generator(nn.Module):
     def __init__(self, dim_z=100, channels = [64, 128, 256]):
         super().__init__()
         self.dim_z = dim_z
-
-        ##################
-        # TODO: Implement the Generator architecture
-        # Hint: use three fully connected layers with LeakyReLU activations.
-        #       The final output layer should map to 784 and use Tanh.
-        ##################
         self.model = nn.Sequential(
             # TODO: fill in layers here
             nn.Linear(self.dim_z, channels[0]),
@@ -61,7 +48,7 @@ class Generator(nn.Module):
             nn.Linear(channels[2], 784),
             nn.Tanh(),
 
-            nn.Identity()  # placeholder
+            nn.Identity()  
         )
 
     def forward(self, x):
@@ -75,28 +62,18 @@ def train_discriminator(discriminator, d_optimizer, images, fake_images, criteri
     discriminator.zero_grad()
     outputs_real = discriminator(images)
 
-    ############################
-    # TODO: Create labels
-    # Hint: real_labels should be all 1s, fake_labels all 0s
-    ############################
     real_labels = torch.ones(images.size(0), 1).to(device)
     fake_labels = torch.zeros(fake_images.size(0), 1).to(device)
 
-    ############################
-    # TODO: Compute loss on real images
-    ############################
+
     real_loss = criterion(outputs_real, real_labels)
 
     outputs_fake = discriminator(fake_images.detach())
 
-    ############################
-    # TODO: Compute loss on fake images
-    ############################
+
     fake_loss = criterion(outputs_fake, fake_labels)
 
-    ############################
-    # TODO: Combine real and fake loss
-    ############################
+  
     d_loss = real_loss + fake_loss
     
     d_loss.backward()
@@ -107,19 +84,9 @@ def train_discriminator(discriminator, d_optimizer, images, fake_images, criteri
 def train_generator(generator, g_optimizer, discriminator_outputs, criterion, device):
     
     generator.zero_grad()
-
-    ############################
-    # TODO: Generator wants to fool the discriminator
-    # Hint: use real_labels = all 1s (pretend fake images are real)
-    ############################
     real_labels =  torch.ones(discriminator_outputs.size(0), 1).to(device)
 
-    ############################
-    # TODO: Compute generator loss
-    # Hint: criterion(discriminator_outputs, real_labels)
-    ############################
     g_loss = criterion(discriminator_outputs, real_labels)
-
     g_loss_fake = criterion(discriminator_outputs, 1 - real_labels.view(-1, 1))
     g_loss.backward()
     g_optimizer.step()
